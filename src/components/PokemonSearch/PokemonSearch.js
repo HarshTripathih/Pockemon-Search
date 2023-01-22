bf9783324f6c74f079d139e4b079703447568ba9
HarshTripathih <harshtripathih321@gmail.com>
2023-01-22 17:33:23 +0530

    done Pokemon

---
+++ src/components/PokemonSearch/PokemonSearch.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function PokemonSearch({ filter, setFilter }) {
+
+    const searchPokemon = (event) => {
+        setFilter(event.target.value)
+    }
+    return (
+        <input
+            type='text'
+            value={filter}
+            onChange={searchPokemon}
+
+        />
+    )
+}
+
+export default PokemonSearch
